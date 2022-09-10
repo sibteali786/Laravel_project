f4ce4efca1e786d1eb44a3120c11f085a221729e
Syed_Sibteali_Baqar <sibteali786@gmail.com>
2022-09-11 00:51:33 +0500

    Controler and Routing concept
    1. What is Routing
    2. How to make routing
    3. How to pass params in url
    4. What is controler, how to make custom controller and pass params to
       it.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('hello');
+    return view('welcome');
 });
+
+Route::view("about", '/about');
+Route::view("contact", '/contact');
+
+Route::get('user/{id}', [UserController::class, 'show']);
